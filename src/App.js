6bf87bc12d6e1d9d549831102d7bf67df4e2e5ff
syrinemahmoud <56481359+syrinemahmoud@users.noreply.github.com>
2019-10-26 16:21:59 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './style.css';
+import myImage from './welcome2.png';
+import MyComponent from './MyComponent';
+import Container from './Container';
+
+
+function App(){
+  return(
+    <div className="Hello">
+        
+       
+        <Container/>
+        
+      </div>
+  )
+  
+}
+
+ 
+
+export default App;
+
+    
+    
+
+
+  
+
+
+/*
+<div style={{
+  border: 'solid 1px black',
+  maxwidth:'100vw'
+}}>
+  
+  <h1 className=" title Hello"
+    style={{
+      color:'red',
+      fontSize:'90px',
+      box:'14px 50px'
+    }}>
+  </h1>
+  <h2 className="red"
+  style={{
+
+  }}>
+
+  </h2>
+</div>/*
+
+
+
+
+/*
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
